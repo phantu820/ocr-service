cd1ddee05752e1c9db709c2e44dc6daba384d18f
leopan <tu416164@gmail.com>
2024-07-13 15:07:36 +0700

    notification backend

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
-import express from 'express';
-import Tesseract from 'tesseract.js';
-import { Readable } from 'stream';
-import mainFun from './ocr.js';
-import  multer from 'multer'
-const upload = multer({ dest: 'uploads/' })
+import express from "express";
+import mainFun from "./ocr.js";
+import multer from "multer";
+import { Expo } from "expo-server-sdk";
+const upload = multer({ dest: "uploads/" });
 const app = express();
+const expo = new Expo();
 
 app.use(express.json());
 
+app.get("/ocr", (req, res) => {
+  res.send("Hello, OCR Server!");
+});
 
-app.get('/ocr', (req, res) => {
-  res.send('Hello, OCR Server!');
-})
-
-app.post('/ocr',upload.array('images'),  async (req, res) => {
+app.post("/ocr", upload.array("images"), async (req, res) => {
   // console.log('Uploading images', req.files);
   try {
-    const images = req.files
-  const data = await Promise.all(images.map(async (e) =>await mainFun(e.path)))
+    const images = req.files;
+    const data = await Promise.all(
+      images.map(async (e) => await mainFun(e.path))
+    );
     console.log(data);
-    res.json(data.reduce((c,a) => c + a,""));
+    res.json(data.reduce((c, a) => c + a, ""));
   } catch (error) {
     res.json("error", error);
   }
 });
 
+app.post("/send-notification", async (req, res) => {
+  const { to, title, body, data } = req.body;
+  console.log("token", to);
+
+  let messages = [];
+  for (let pushToken of to) {
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    messages.push({
+      to: pushToken,
+      sound: "default",
+      title,
+      body,
+      data,
+    });
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  try {
+    for (let chunk of chunks) {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      tickets.push(...ticketChunk);
+    }
+    res.status(200).send(tickets);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 app.listen(2002, () => {
-  console.log('Server is running on port 2002');
+  console.log("Server is running on port 2002");
 });
--- ocr.js
+++ ocr.js
@@ -1,15 +1,11 @@
-
-import tesseract from "node-tesseract-ocr"
-import fs from "fs/promises"
-import { createWorker } from 'tesseract.js';
+import { createWorker } from "tesseract.js";
 const scanText = async (img) => {
-  const worker = await createWorker('vie');
+  const worker = await createWorker("vie");
   const ret = await worker.recognize(img);
   await worker.terminate();
-  return ret.data.text
-
-}
+  return ret.data.text;
+};
 const mainFun = async (img) => {
-    return await scanText(img);
-} 
-export default  mainFun;
+  return await scanText(img);
+};
+export default mainFun;
