920bf057d38bf57c16d054257535c4f24cffa716
leopan <tu416164@gmail.com>
2024-08-07 22:52:00 +0700

    deploy

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ app.post("/ocr", upload.array("images"), async (req, res) => {
         return result;
       })
     );
-    // Trả về kết quả dưới dạng văn bản
     res.json(data.reduce((acc, text) => acc + text, ""));
   } catch (error) {
     console.error("Error processing images:", error);
@@ -35,46 +34,46 @@ app.post("/ocr", upload.array("images"), async (req, res) => {
   }
 });
 
-app.post("/send-notification", async (req, res) => {
-  const { to, title, body, data } = req.body;
-  console.log("token", to);
+// app.post("/send-notification", async (req, res) => {
+//   const { to, title, body, data } = req.body;
+//   console.log("token", to);
 
-  if (!Array.isArray(to) || !title || !body) {
-    return res.status(400).json({ error: "Invalid request payload" });
-  }
+//   if (!Array.isArray(to) || !title || !body) {
+//     return res.status(400).json({ error: "Invalid request payload" });
+//   }
 
-  let messages = [];
-  for (let pushToken of to) {
-    if (!Expo.isExpoPushToken(pushToken)) {
-      console.error(`Push token ${pushToken} is not a valid Expo push token`);
-      continue;
-    }
+//   let messages = [];
+//   for (let pushToken of to) {
+//     if (!Expo.isExpoPushToken(pushToken)) {
+//       console.error(`Push token ${pushToken} is not a valid Expo push token`);
+//       continue;
+//     }
 
-    messages.push({
-      to: pushToken,
-      sound: "default",
-      title,
-      body,
-      data,
-    });
-  }
+//     messages.push({
+//       to: pushToken,
+//       sound: "default",
+//       title,
+//       body,
+//       data,
+//     });
+//   }
 
-  let chunks = expo.chunkPushNotifications(messages);
-  let tickets = [];
-  try {
-    for (let chunk of chunks) {
-      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-      tickets.push(...ticketChunk);
-    }
-    console.log("Notifications sent successfully");
-    res.status(200).json(tickets);
-  } catch (error) {
-    console.error("Error sending notifications:", error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while sending notifications." });
-  }
-});
+//   let chunks = expo.chunkPushNotifications(messages);
+//   let tickets = [];
+//   try {
+//     for (let chunk of chunks) {
+//       let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+//       tickets.push(...ticketChunk);
+//     }
+//     console.log("Notifications sent successfully");
+//     res.status(200).json(tickets);
+//   } catch (error) {
+//     console.error("Error sending notifications:", error);
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while sending notifications." });
+//   }
+// });
 
 app.listen(2002, () => {
   console.log("Server is running on port 2002");
